a33b1bc82f50b54f3c9635500f60a89123c7aae3
sahara <jwk0528@163.com>
2022-06-22 15:45:44 +0800

    fix: 内网机器IP跳转修改

--- src/components/MachineWebLink/index.tsx
+++ src/components/MachineWebLink/index.tsx
@@ -11,9 +11,21 @@ export const handleIpHerf = async (ip: string, type: string) => {
         }
         requestCodeMessage(code, msg)
     } else {
-        return
-        // const href = getTerminalLink(ip)
-        // const win: any = window.open("");
-        // setTimeout(function () { win.location.href = href })
+        // return
+        // 内网机器IP/SN跳转terminal处理方法 勿删除
+        const href = getTerminalLink(ip)
+        const win: any = window.open("");
+        setTimeout(function () { win.location.href = href })
     }
+}
+// 内网机器IP/SN跳转terminal处理方法 勿删除
+export const getTerminalLink = (link: string) => {
+    const src = "https://sa.alibaba-inc.com/ops/terminal.html?&source=tone&"
+    const ipRgx = /^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$/
+
+    if (link) {
+        if (ipRgx.test(link)) return src + `ip=${link}`
+        return src + `sn=${link}`
+    }
+    return link
 }
