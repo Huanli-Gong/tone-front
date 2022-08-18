ec46dd57af87ac989d10f20bdea73327ebf0ab0d
wulibaibao <13366578180@163.com>
2022-08-18 10:29:37 +0800

    refactor: nav test job hover menu refactor UI

--- src/components/Header/components/JobModal.tsx
+++ src/components/Header/components/JobModal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Layout, Row, Input, Tooltip, message, Spin, Tag } from 'antd'
+import { Layout, Row, Input, Tooltip, message, Spin, Tag, Col, Typography, Space } from 'antd'
 import { switchServerType, switchChineseType } from '@/utils/utils'
 import { history, useModel } from 'umi'
 import styles from './jobModel.less'
@@ -51,7 +51,7 @@ export default (props: any) => {
         initTemplate()
     }, [initialState?.refreshMenu, ws_id])
     useEffect(() => {
-        if(pageParams.name) {
+        if (pageParams.name) {
             initTemplate()
             setPageParams({ ...defaultParams })
         }
@@ -63,18 +63,18 @@ export default (props: any) => {
             queryTemplate({ ...pageParams, page_num: pageParams.page_num + 1 })
         }
     }
-    const TootipOver : React.FC<any> = (props:any) => {
+    const TootipOver: React.FC<any> = (props: any) => {
         let content = props.children
-        if(content.length > 23){
+        if (content.length > 23) {
             return <Tooltip title={content}>
                 {content}
             </Tooltip>
         }
-            return content
+        return content
     }
     const dataSourceCopy = props.getData(dataSource)
     return (
-        <Layout.Content style={{ minWidth : 468 , maxWidth: 912 }} key={props.testType}>
+        <Layout.Content style={{ minWidth: 468, maxWidth: 912, paddingBottom: 8 }} key={props.testType}>
             <Row style={{ paddingLeft: 20, paddingRight: 19, marginBottom: 16 }}>
                 <Input
                     autoComplete="off"
@@ -85,41 +85,78 @@ export default (props: any) => {
                 />
             </Row>
             <Spin spinning={loading}>
-                {
-                    dataSourceCopy.length > 0 &&
-                    <div className={styles.job_type}>
-                            {
-                                dataSourceCopy.map(
-                                    (item: any, index: number) => (
-                                        <div
-                                            key={index}
-                                            className={styles.type_modal_item}
-                                            style={{ margin: dataSourceCopy.length > 12 ? '0 7px 16px 7px' : '0 8px 16px 8px' }}
-                                            onClick={() => {
-                                                if (!item.job_type) message.error('问题模板，请及时删除')
-                                                else {
-                                                    props.onOk()
-                                                    history.push(`/ws/${ws_id}/test_job/${item.job_type_id}?template_id=${item.id}`)
-                                                }
-                                            }}
+                <div
+                    style={{ width: 912, maxHeight: 320, overflowX: "hidden", overflowY: "scroll" }}
+                >
+                    {
+                        dataSourceCopy.length > 0 &&
+                        dataSourceCopy.map(
+                            (item: any) => (
+                                <Row
+                                    key={item.id}
+                                    style={{
+                                        paddingLeft: 20,
+                                        paddingRight: 20,
+                                        cursor: "pointer"
+                                    }}
+                                    onClick={(): any => {
+                                        if (!item.job_type)
+                                            return message.error('问题模板，请及时删除')
+                                        props.onOk()
+                                        history.push(`/ws/${ws_id}/test_job/${item.job_type_id}?template_id=${item.id}`)
+                                    }}
+                                >
+                                    <Col
+                                        span={24}
+                                        style={{
+                                            borderTop: "1px solid rgba(0,0,0,.06)",
+                                            paddingTop: 5,
+                                            paddingBottom: 5,
+                                        }}
+                                    >
+                                        <Space
+                                            style={{ width: "100%" }}
+                                            direction="vertical"
+                                            size={4}
                                         >
-                                            <div className={styles.type_modal_title}>
-                                                <TootipOver>{item.name}</TootipOver>
-                                            </div>
-                                            <div style={{ paddingLeft: 16 }}>
-                                                <Tag color="#F2F4F6" style={{ color: '#515B6A' }}>{switchServerType(item.server_provider)}</Tag>
-                                                <Tag color="#F2F4F6" style={{ color: '#515B6A' }}>{switchChineseType(item.test_type)}</Tag>
-                                            </div>
-                                        </div>
-                                    )
-                                )
-                            }
-                    </div>
-                }
+                                            <Typography.Text
+                                                style={{
+                                                    wordBreak: "break-all",
+                                                    color: "rgba(0,0,0,.65)"
+                                                }}
+                                            >
+                                                {item.name}
+                                            </Typography.Text>
+                                            <Space>
+                                                {
+                                                    [
+                                                        ["server_provider", switchServerType],
+                                                        ["test_type", switchChineseType]
+                                                    ].map((fields: any) => {
+                                                        const [field, func] = fields
+                                                        return (
+                                                            <Tag
+                                                                key={field}
+                                                                color="#F2F4F6"
+                                                                style={{ color: '#515B6A' }}
+                                                            >
+                                                                {func(item[field])}
+                                                            </Tag>
+                                                        )
+                                                    })
+                                                }
+                                            </Space>
+                                        </Space>
+                                    </Col>
+                                </Row>
+                            )
+                        )
+                    }
+                </div>
                 {
                     dataSourceCopy.length === 0 &&
-                    <div style={{ height:80, lineHeight:'80px', textAlign: 'center', color: 'rgba(0,0,0,.35)' }}>
-                        <FrownOutlined style={{ marginRight:8 }}/>暂无模板
+                    <div style={{ height: 80, lineHeight: '80px', textAlign: 'center', color: 'rgba(0,0,0,.35)' }}>
+                        <FrownOutlined style={{ marginRight: 8 }} />暂无模板
                     </div>
                 }
             </Spin>
