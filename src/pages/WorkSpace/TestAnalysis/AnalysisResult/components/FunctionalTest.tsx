c97d3e1fbeb6612e97b317f1a6b41004ce4a238d
wulibaibao <13366578180@163.com>
2022-12-26 10:10:49 +0800

    fix: 合并问题修复

--- src/pages/WorkSpace/TestAnalysis/AnalysisResult/components/FunctionalTest.tsx
+++ src/pages/WorkSpace/TestAnalysis/AnalysisResult/components/FunctionalTest.tsx
@@ -259,14 +259,15 @@ const ReportTestFunc: React.FC<any> = (props) => {
                                                                 </CaseTitle>
                                                                 {
                                                                     conf_data?.slice(0, group)?.map((metric: any, idx: number) => (
-                                                                            <CaseText gLen={group} key={idx}>
-                                                                                <Space size={16}>
-                                                                                    <Typography.Text style={{ color: '#649FF6' }}>{toShowNum(metric.all_case)}</Typography.Text>
-                                                                                    <Typography.Text style={{ color: '#81BF84' }}>{toShowNum(metric.success_case)}</Typography.Text>
-                                                                                    <Typography.Text style={{ color: '#C84C5A' }}>{toShowNum(metric.fail_case)}</Typography.Text>
-                                                                                </Space>
-                                                                                {metric && !metric.is_baseline ?
-                                                                                    <JumpResult ws_id={wsId} job_id={metric.obj_id} style={{ paddingLeft: 10 }}/> : <>-</>
+                                                                        <CaseText gLen={group} key={idx}>
+                                                                            <Space size={16}>
+                                                                                <Typography.Text style={{ color: '#649FF6' }}>{toShowNum(metric.all_case)}</Typography.Text>
+                                                                                <Typography.Text style={{ color: '#81BF84' }}>{toShowNum(metric.success_case)}</Typography.Text>
+                                                                                <Typography.Text style={{ color: '#C84C5A' }}>{toShowNum(metric.fail_case)}</Typography.Text>
+                                                                                {
+                                                                                    metric && !metric.is_baseline ?
+                                                                                        <JumpResult ws_id={wsId} job_id={metric.obj_id} style={{ paddingLeft: 10 }} /> :
+                                                                                        <>-</>
                                                                                 }
                                                                             </Space>
                                                                         </CaseText>
