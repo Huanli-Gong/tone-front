6f42925c93de5dcf1ad10a8cb01f94434e9ca209
wulibaibao <13366578180@163.com>
2023-02-28 10:17:26 +0800

    fix: 基线管理删除最后一条检测分页值

--- src/components/CommonPagination/index.tsx
+++ src/components/CommonPagination/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useIntl, FormattedMessage } from 'umi'
-import { Pagination , Row , Col } from 'antd'
+import { useIntl } from 'umi'
+import { Pagination, Row, Col } from 'antd'
 import styled from 'styled-components'
 
 const CommonPagination = styled(Row)`
@@ -10,38 +10,38 @@ const CommonPagination = styled(Row)`
 `
 
 interface PaginationProps {
-    total : number,
-    pageSize : number ,
-    currentPage : number,
-    onPageChange : ( page : number , size? : number ) => void,
-    style?:any,
-    largePage?:boolean,
-} 
+    total: number,
+    pageSize: number,
+    currentPage: number,
+    onPageChange: (page: number, size?: number) => void,
+    style?: any,
+    largePage?: boolean,
+}
 
-export default ( props : PaginationProps ) => {
+export default (props: PaginationProps) => {
     const { formatMessage } = useIntl()
-    const { total = 0 , largePage = false, pageSize = 10 , currentPage = 1 , onPageChange , ...rest } = props 
-    let sizeNumber = ['10','20','50','100']
-    if ( total === 0  ) return <CommonPagination className="commom_pagination" justify="space-around" {...rest} />
-    if( largePage ) {
-        sizeNumber = sizeNumber.concat(['200','500'])
+    const { total = 0, largePage = false, pageSize = 10, currentPage = 1, onPageChange, ...rest } = props
+    let sizeNumber = ['10', '20', '50', '100']
+    if (total === 0) return <CommonPagination className="commom_pagination" justify="space-around" {...rest} />
+    if (largePage) {
+        sizeNumber = sizeNumber.concat(['200', '500'])
     }
     return (
         <CommonPagination className="commom_pagination" justify="space-around" {...rest}>
-            <Col span={ 4 } style={{ textAlign : 'left' }}>
-               {formatMessage({id: 'pagination.total.strip'}, {data: total})}
+            <Col span={4} style={{ textAlign: 'left' }}>
+                {formatMessage({ id: 'pagination.total.strip' }, { data: total })}
             </Col>
-            <Col span={ 20 } style={{ textAlign : 'right' }}>
-                <Pagination 
-                    total={ total }
-                    pageSize={ pageSize }
-                    current={ currentPage }
+            <Col span={20} style={{ textAlign: 'right' }}>
+                <Pagination
+                    total={total}
+                    pageSize={pageSize}
+                    current={currentPage}
                     size="small"
                     showSizeChanger
                     pageSizeOptions={sizeNumber}
-                    onChange={ onPageChange }
-                    showQuickJumper={ true }
-                    onShowSizeChange={ onPageChange }
+                    onChange={onPageChange}
+                    showQuickJumper={true}
+                    onShowSizeChange={onPageChange}
                 />
             </Col>
         </CommonPagination>
