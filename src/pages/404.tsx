81536faf9de8d438eab91f341aa191ee8dd6ff29
wulibaibao <13366578180@163.com>
2023-03-06 11:35:16 +0800

    fix: ws失效页面返回按钮跳转至系统首页

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import { Layout, Space, Typography } from 'antd';
 import React from 'react';
 import Icon from '@/assets/img/loss.png';
 import { useClientSize } from '@/utils/hooks';
-import { useIntl, useParams, history, Helmet } from 'umi';
+import { useIntl, useParams, history, Helmet, useModel } from 'umi';
 
 import { Container, Wrapper } from "./500"
 
@@ -10,6 +10,16 @@ const NoFoundPage: React.FC<{}> = () => {
     const intl = useIntl()
     const { ws_id } = useParams() as any
     const { height } = useClientSize()
+
+    const { initialState: { authList } } = useModel("@@initialState")
+
+    const handleClickBack = () => {
+        if (!authList.ws_is_exist) {
+            history.push(`/`)
+            return
+        }
+        history.push(ws_id ? `/ws/${ws_id}/test_result` : `/`)
+    }
 
     return (
         <Container height={height - 50}>
@@ -31,7 +41,7 @@ const NoFoundPage: React.FC<{}> = () => {
                                 <div style={{ color: '#000', fontSize: 16, opacity: 0.45, }}>页面链接可能已失效或被删除</div>
                             </Space>
                             <Typography.Link
-                                onClick={() => history.push(ws_id ? `/ws/${ws_id}/test_result` : `/`)}
+                                onClick={handleClickBack}
                             >
                                 {intl.formatMessage({ defaultMessage: "返回首页", id: "page.500.button" })}
                             </Typography.Link>
