e0be43341b25c51d65e82910b17f5ea239c6465a
sahara <jwk0528@163.com>
2023-08-30 14:21:03 +0800

    fix: 文案更新

--- src/components/Disclaimer/index.tsx
+++ src/components/Disclaimer/index.tsx
@@ -36,8 +36,8 @@ const Disclaimer: React.FC<any> = (props) => {
             >
                 <div style={{ margin: '16px 0px', fontSize: 14 }}>
                     <TextWrap style={{  fontWeight: 'bold',fontSize: 18, marginBottom: 10 }}>免责声明：</TextWrap>
-                    <TextWrap><b>1.</b>&nbsp;当前测试平台所提供的服务仅供在测试环境使用，<b>本平台不承诺对用户</b>测试机器的稳定性保障。</TextWrap>
-                    <TextWrap><b>2.</b>&nbsp;若用户私自通过任何形式把测试或生产机器添加到本平台执行测试并影响到业务运行的，本测试平台及测试平台的<b>研发团队概不负责。</b></TextWrap>
+                    <TextWrap><b>1.</b>&nbsp;当前测试平台所提供的服务仅供在测试环境使用，<b>本平台不承诺对用户测试机器的稳定性保障</b>。</TextWrap>
+                    <TextWrap><b>2.</b>&nbsp;若用户自行把测试或生产机器加到本平台执行测试并影响到业务运行的，<b>本测试平台及平台的研发团队概不负责</b>。</TextWrap>
                 </div>
             </Modal>
         </>
