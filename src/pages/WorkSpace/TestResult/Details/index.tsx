23958d66b624b72702258b1735d21d5b80a41722
tianweiweiliang <875658254@qq.com>
2024-04-03 15:16:06 +0800

    fix: 测试过程-测试准备失败原因添加hover

--- src/pages/WorkSpace/TestResult/Details/index.tsx
+++ src/pages/WorkSpace/TestResult/Details/index.tsx
@@ -24,7 +24,6 @@ import { isNull } from 'lodash'
 import { BreadcrumbItem, CAN_STOP_JOB_STATES, RenderDesItem, EditNoteBtn } from "./components/MainPageComponents"
 
 const TextStyle: any = {
-    // width: 'calc(100% - 104px)',
     wordBreak: 'break-all',
     whiteSpace: 'pre-wrap',
     overflow: 'hidden',
